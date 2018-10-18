b58ed9e58abf686ef0332265be30f98fbf4f5b0d
AnthonySica <asica@gfhworkshop.com>
2018-10-18 01:53:58 +0000

    first commit

---
+++ Intuit/app/config.py
@@ -0,0 +1,39 @@
+DEBUG = False
+SQLALCHEMY_ECHO = False
+
+ALLOWED_HOSTS = [
+    'localhost'
+    '0.0.0.0'
+]
+
+
+# Specify which OAuth your app uses; default is OAuth2
+# Change this flag to 1 for OAUth1 apps
+AUTH_TYPE = 'OAuth2'
+# AUTH_TYPE = 'OAuth1'
+
+# OAuth2
+# CLIENT_ID= 'EnterClientIDHere'
+# CLIENT_SECRET = 'EnterClientSecretHere'
+# REDIRECT_URI = 'http://localhost:5000/callback'
+
+CLIENT_ID = 'Q04jb9uEi7zIAA0kmSnUnhNKH6CyMa2YT5RNY1svI4es3Cnfwf'
+CLIENT_SECRET = 'Lm1NSuNsA4jANoGOke4HxVip5IBmMDgmoz70J7pp'
+REDIRECT_URI = 'http://gfhdev.local:8000/callback'
+
+# OAuth1
+CONSUMER_KEY = 'EnterConsumerKeyHere'
+CONSUMER_SECRET = 'EnterConsumerSecretHere'
+
+# OAuth1 Base URLs
+OAUTH1_BASE = 'https://oauth.intuit.com'
+REQUEST_TOKEN_URL = 'https://oauth.intuit.com/oauth/v1/get_request_token'
+ACCESS_TOKEN_URL = 'https://oauth.intuit.com/oauth/v1/get_access_token'
+AUTHORIZE_URL = 'https://appcenter.intuit.com/Connect/Begin'
+
+# Choose environment; default is sandbox
+ENVIRONMENT = 'Sandbox'
+# ENVIRONMENT = 'Production'
+
+# Set to latest at the time of updating this app, can be be configured to any minor version
+API_MINORVERSION = '23'
